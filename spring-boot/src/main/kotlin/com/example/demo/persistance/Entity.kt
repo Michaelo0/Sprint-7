e404018772452fe596352122d833b95cde8beff4
Vincent Marillion <vmarilli@student.21-school.ru>
2021-11-07 16:48:43 +0300

    springboot task

--- spring-boot/src/main/kotlin/com/example/demo/persistance/Entity.kt
+++ spring-boot/src/main/kotlin/com/example/demo/persistance/Entity.kt
@@ -1,3 +1,16 @@
 package com.example.demo.persistance
 
-class Entity
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+class Entity(name: String) {
+    @Id
+    @GeneratedValue
+    var id: Long? = null
+
+    @Column
+    var name: String? = null
+}
--- spring-boot/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ spring-boot/src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,27 @@
 package com.example.demo
 
+import com.example.demo.persistance.Entity
+import com.example.demo.persistance.EntityRepository
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private lateinit var entityRepository: EntityRepository
+
 	@Test
-	fun contextLoads() {
+	fun test() {
+		val savedEntity = entityRepository.save(Entity(name = "name"))
+
+		val foundEntity = savedEntity.id?.let { entityRepository.findById(it) }
+
+		if (foundEntity != null) {
+			assertTrue(foundEntity.get() == savedEntity)
+		}
 	}
 
 }
